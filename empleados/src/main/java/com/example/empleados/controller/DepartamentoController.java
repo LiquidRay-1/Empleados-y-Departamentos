15d26351402d5d41fb0105aaa1ccc618d864874b
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-01-16 20:06:44 +0100

    Add files via upload

--- empleados/src/main/java/com/example/empleados/controller/DepartamentoController.java
+++ empleados/src/main/java/com/example/empleados/controller/DepartamentoController.java
@@ -3,11 +3,9 @@ package com.example.empleados.controller;
 import com.example.empleados.model.Departamento;
 import com.example.empleados.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin({"*"})
 @RestController
 @RequestMapping("/api/departamento")
 public class DepartamentoController {
